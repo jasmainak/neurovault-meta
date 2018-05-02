45b62a7f419e54ad4ddecc68b35d0ea3a1d6de6e
Mainak Jas <mainakjas@gmail.com>
2018-05-02 19:15:02 +0200

    todos

--- reduce_dimensionality.py
+++ reduce_dimensionality.py
@@ -8,7 +8,10 @@ from nilearn.image import resample_to_img
 # TODOs
 #
 # 1. feature extraction / metadata
-# 2. dimensionality reduction
+# 2. t maps to z maps etc.
+# 3. weighting of different collections
+#    (some are subject level, some are group level)
+# 4. weighting features -- e.g., cog atlas terms
 
 # get data / list of files
 nv_data = datasets.fetch_neurovault(max_images=100)
@@ -26,7 +29,7 @@ maps = nib.load('components_512.nii.gz')
 # find images with same affine
 imgs = []
 for ii, image in enumerate(images):
-    print(ii)
+    print('Resampling image %d' % ii)
     img = nib.load(image)
     img = resample_to_img(img, maps)
     imgs.append(img)
