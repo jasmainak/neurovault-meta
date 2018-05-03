be4a09124fad8d6a587d6dffb5c5e0adb949c883
Mainak Jas <mainakjas@gmail.com>
2018-05-03 18:06:25 +0200

    Resample once

---
+++ resample_and_save.py
@@ -0,0 +1,30 @@
+import os
+import os.path as op
+
+from nilearn.image import resample_to_img
+from nilearn import datasets
+
+import nibabel as nib
+
+base_dir = 'neurovault_resampled'
+nv_data = datasets.fetch_neurovault(max_images=None, mode='offline')
+
+images = nv_data['images']
+
+if not op.exists(base_dir):
+    os.mkdir(base_dir)
+
+target_img = nib.load(images[0])
+for ii, image in enumerate(images):
+    collection, name = image.split('/')[-2:]
+    fname = op.join('neurovault_resampled',
+                    collection, name)
+    print('Resampling image %d' % ii)
+    if op.exists(fname):
+        continue
+
+    if not op.exists(op.join(base_dir, collection)):
+        os.mkdir(op.join(base_dir, collection))
+    img = nib.load(image)
+    img = resample_to_img(img, target_img)
+    nib.save(img, fname)
