99417090286a65450e49ae58de39dc002694846d
Mainak Jas <mainakjas@gmail.com>
2018-05-03 10:32:08 +0200

    Simple contrast for motor

---
+++ encoder.py
@@ -0,0 +1,47 @@
+import nibabel as nib
+import numpy as np
+import pandas as pd
+
+from nilearn import datasets
+from nilearn.image import resample_to_img
+from nilearn.plotting import plot_glass_brain
+
+# get data / list of files
+nv_data = datasets.fetch_neurovault(max_images=None,
+                                    mode='offline')
+
+# just average maps containing a certain term
+
+images = nv_data['images']
+images_meta = nv_data['images_meta']
+collections = nv_data['collections_meta']
+
+# export metadata to pandas
+metadata_df = pd.DataFrame(images_meta)
+
+metadata_df['is_motor'] = metadata_df['name'].apply(lambda x: 'motor' in x)
+
+motor_idxs = np.where(metadata_df['is_motor'])[0]
+other_idxs = np.where(np.invert(metadata_df['is_motor']))[0]
+
+images_motor = [images[idx] for idx in motor_idxs]
+images_other = [images[idx] for idx in other_idxs]
+
+
+def average_maps(images, target_img):
+    avg = np.zeros_like(target_img.dataobj)
+    for ii, image in enumerate(images):
+        print('Resampling image %d' % ii)
+        img = nib.load(image)
+        img = resample_to_img(img, target_img)
+        avg += img.dataobj
+    avg /= len(images)
+    return avg
+
+
+target_img = nib.load(images[0])
+avg_motor = average_maps(images_motor, target_img)
+avg_other = average_maps(images_other, target_img)
+contrast = nib.Nifti1Image(avg_motor - avg_other, target_img.affine)
+
+plot_glass_brain(contrast, plot_abs=True)
