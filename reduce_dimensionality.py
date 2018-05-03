5be95d63b3b1f7a6f158f38099b0a0fa3192357b
Mainak Jas <mainakjas@gmail.com>
2018-05-04 01:00:53 +0200

    more

--- reduce_dimensionality.py
+++ reduce_dimensionality.py
@@ -5,6 +5,8 @@ from nilearn import datasets
 from nilearn.input_data import NiftiMapsMasker
 from nilearn.image import resample_to_img
 
+data_dir = '/home/mainak/Desktop/neurovault/'
+
 # TODOs
 #
 # 1. feature extraction / metadata
@@ -14,9 +16,15 @@ from nilearn.image import resample_to_img
 # 4. weighting features -- e.g., cog atlas terms
 # 5. dirty_cat for labels using ngram similarities (SimilarityEncoder)
 # 6. Encoding problem = terms to maps. Decoding problem = maps to terms
+# 7. Variance across collections ...
+# 8. Normalize before averaging? Remove p values ...
+# 9. Save resampled files
+# 10. Distribution of #images per collection
+# 11. Average over all images?
 
 # get data / list of files
-nv_data = datasets.fetch_neurovault(max_images=100)
+nv_data = datasets.fetch_neurovault(max_images=None, mode='offline',
+                                    data_dir=data_dir)
 
 images = nv_data['images']
 images_meta = nv_data['images_meta']
