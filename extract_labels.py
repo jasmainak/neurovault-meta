598267ac5578fce09b5cb7324b8225e3947665b0
Mainak Jas <mainakjas@gmail.com>
2018-05-04 11:57:58 +0200

    Find most common labels

--- extract_labels.py
+++ extract_labels.py
@@ -13,15 +13,33 @@ collections = nv_data['collections_meta']
 
 # export metadata to pandas
 metadata_df = pd.DataFrame(images_meta)
-metadata_df['name'] = metadata_df['name'].apply(lambda x: x.replace('_', ' '))
+# metadata_df['name'] = metadata_df['name'].apply(lambda x: x.replace('_', ' '))
 
-se = SimilarityEncoder(similarity='ngram', handle_unknown='ignore')
+string_columns = metadata_df.select_dtypes(include=['object']).columns
+# [(col, metadata_df[col].map(type).unique()) for col in string_columns]
+string_columns = string_columns.tolist()
+string_columns.remove('data')
+metadata_df['all'] = metadata_df[string_columns].apply(
+    lambda row: row.str.cat(sep=' '), axis=1)
+
 vectorizer = CountVectorizer()
 
+corpus = [metadata_df.iloc[ii]['all'] for ii in range(metadata_df.shape[0])]
+bag_of_words = vectorizer.fit_transform(corpus)
+sum_words = bag_of_words.sum(axis=0)
+words_freq = [(word, sum_words[0, idx])
+              for word, idx in vectorizer.vocabulary_.items()]
+words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
+frequent_words = [w[0] for w in words_freq]
+
+print(' '.join(frequent_words[:100]))
+
+sdfdf
+
+se = SimilarityEncoder(similarity='ngram', handle_unknown='ignore')
 y = se.fit_transform(metadata_df.name) # XXX: need more features than 1 ...
 
 sdfdf
-y = vectorizer.fit_transform(metadata_df.name)
 
 # look at the metadata
 if True:
